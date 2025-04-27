349a0e75ca06517304e20c773cb45805d54365bd
Laura Sarmiento <l.sarmientog@uniandes.edu.co>
2025-04-27 18:53:58 -0500

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SerieModule } from './serie/serie.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,11 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SerieModule,
+    HttpClientModule
   ],
-  providers: [
-    provideClientHydration(withEventReplay())
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/serie/serie.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Serie } from './serie';
+import { dataSeries } from './dataSeries';
+import { CommonModule } from '@angular/common';
+import { SerieService } from './serie.service';
+
+@Component({
+  selector: 'app-serie',
+  templateUrl: './serie.component.html',
+  styleUrls: ['./serie.component.css'],
+  imports: [CommonModule]
+})
+export class SerieComponent implements OnInit {
+
+  series: Array<Serie> = [];
+  promedio: number = 0;
+  constructor(private serieService: SerieService) { }
+
+  getSeries() {
+    this.serieService.getSeries().subscribe(series => {
+      this.series = series;
+      this.getPromedio();
+    });
+  }
+
+  getPromedio() {
+    let suma = 0;
+    for (let s of this.series) {
+      suma += s.seasons;
+    }
+
+    this.promedio = suma / this.series.length;
+  }
+
+  selectedSerie: Serie | null = null;
+
+  showSeriesDetail(serie: Serie) {
+    this.selectedSerie = serie;
+  }
+
+  ngOnInit() {
+    this.getSeries();
+  }
+
+}
